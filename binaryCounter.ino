53f913d37cb68628e78810e096ad2fb5defbd3ce
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2014-04-07 13:49:14 -0600

    increment button

--- binaryCounter.ino
+++ binaryCounter.ino
@@ -2,22 +2,25 @@ int LED_DIGIT_1 = 2;
 int LED_DIGIT_2 = 3;
 int LED_DIGIT_4 = 4;
 int LED_DIGIT_8 = 5;
+int BUTTON_1 = 7;
 int MAX_DISPLAY_VALUE = 15;
 
-int counter = 0;
+int counter = 2;
 
 void setup() {
 	allDigitsOff();
 }
 
 void loop() {
-	displayByte(counter);
-	counter++;
-	delay(1000);
-	if(counter > MAX_DISPLAY_VALUE) {
-		counter = 0;
-	}
 	
+	if (isButtonOn()) {
+		if(counter > MAX_DISPLAY_VALUE) {
+			counter = 0;
+		}
+		counter++;
+		displayByte(counter);
+	}
+	delay(100);
 }
 
 void displayByte(unsigned int number) {
@@ -30,56 +33,65 @@ void displayByte(unsigned int number) {
 		if (number >= i) {
 			digitOn(i);
 			number = number - i;
-		} else {
-			digitOff(i);
+			} else {
+				digitOff(i);
+			}
 		}
 	}
-}
 
-void digitOn(unsigned int digit) {
-	switch (digit) {
-		case 1:
-		digitalWrite(LED_DIGIT_1, HIGH);
-		break;
-		case 2:
-		digitalWrite(LED_DIGIT_2, HIGH);
-		break;
-		case 4:
-		digitalWrite(LED_DIGIT_4, HIGH);
-		break;
-		case 8:
-		digitalWrite(LED_DIGIT_8, HIGH);
-		break;
+	void digitOn(unsigned int digit) {
+		switch (digit) {
+			case 1:
+			digitalWrite(LED_DIGIT_1, HIGH);
+			break;
+			case 2:
+			digitalWrite(LED_DIGIT_2, HIGH);
+			break;
+			case 4:
+			digitalWrite(LED_DIGIT_4, HIGH);
+			break;
+			case 8:
+			digitalWrite(LED_DIGIT_8, HIGH);
+			break;
+		}
 	}
-}
 
-void digitOff(unsigned int digit) {
-	switch (digit) {
-		case 1:
-		digitalWrite(LED_DIGIT_1, LOW);
-		break;
-		case 2:
-		digitalWrite(LED_DIGIT_2, LOW);
-		break;
-		case 4:
-		digitalWrite(LED_DIGIT_4, LOW);
-		break;
-		case 8:
-		digitalWrite(LED_DIGIT_8, LOW);
-		break;
+	void digitOff(unsigned int digit) {
+		switch (digit) {
+			case 1:
+			digitalWrite(LED_DIGIT_1, LOW);
+			break;
+			case 2:
+			digitalWrite(LED_DIGIT_2, LOW);
+			break;
+			case 4:
+			digitalWrite(LED_DIGIT_4, LOW);
+			break;
+			case 8:
+			digitalWrite(LED_DIGIT_8, LOW);
+			break;
+		}
 	}
-}
 
-void allDigitsOn() {
-	digitOn(1);
-	digitOn(2);
-	digitOn(4);
-	digitOn(8);
-}
+	void allDigitsOn() {
+		digitOn(1);
+		digitOn(2);
+		digitOn(4);
+		digitOn(8);
+	}
 
-void allDigitsOff() {
-	digitOff(1);
-	digitOff(2);
-	digitOff(4);
-	digitOff(8);
-}
+	void allDigitsOff() {
+		digitOff(1);
+		digitOff(2);
+		digitOff(4);
+		digitOff(8);
+	}
+
+	boolean isButtonOn() {
+		int state = digitalRead(BUTTON_1);
+		if (state == HIGH) {
+			return true;
+			} else {
+				return false;
+			}
+		}
